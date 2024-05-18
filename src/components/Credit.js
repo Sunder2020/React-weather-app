1e5d9a3e823b1f5deb7c68f401dfb7e10b0d27c3
SUNDERBONTHU20 <sunderbonthu7@gmail.com>
2024-05-18 13:45:01 +0530

    code updated

--- src/components/Credit.js
+++ src/components/Credit.js
@@ -1,12 +1,5 @@
 import React from "react";
 
 export default function Credit() {
-  return (
-    <div class="credit">
-    <p>
-      
-       <a href="https://github.com/Sunder2020/React-weather-app">Open sourced on Github.</a> Hosted on Netlify
-    </p>
-  </div>
-  );
+  return <div class="credit"></div>;
 }
